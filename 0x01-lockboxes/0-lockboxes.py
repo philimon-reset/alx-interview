816c024deecaa23fff209e2b4fe4ce6f8fdb487f
Philimon Derib <58902914+philimon-reset@users.noreply.github.com>
2021-12-02 17:00:09 +0300

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,7 @@
 #!/user/bin/python3
+""" Lockboxes """
 def canUnlockAll(boxes):
+    """ LockBoxes Function """
     T = []
     for i in range(1, len(boxes)):
         order = [T.extend(x) for x in boxes[:i] + boxes[i + 1:]]
