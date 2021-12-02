69d9ed64f1e25efd7f81d3c81682f8a393227403
philimon-reset <philimonderib2019@gmail.com>
2021-12-02 10:58:33 +0300

    pep8

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,10 +1,10 @@
 #!/user/bin/python3
 def canUnlockAll(boxes):
-  T = []
-  for i in range(1, len(boxes)):
-    order = [T.extend(x) for x in boxes[:i] + boxes[i + 1:]]
-    if i in T:
-      T = []
-    else:
-      return False
-  return True
+    T = []
+    for i in range(1, len(boxes)):
+        order = [T.extend(x) for x in boxes[:i] + boxes[i + 1:]]
+        if i in T:
+            T = []
+        else:
+            return False
+    return True
