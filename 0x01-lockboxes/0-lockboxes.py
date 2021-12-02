c656d9efca73100fd81849525b1ea7b4b71afcb9
Philimon Derib <58902914+philimon-reset@users.noreply.github.com>
2021-12-02 20:38:29 +0300

    Create 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,34 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 
 """
-  Lockboxes: Write a method that determines if all the boxes can be opened.
+  Lockboxes
+  You have n number of locked boxes in front of you.
+  Each box is numbered sequentially from 0 to n - 1
+  and each box may contain keys to the other boxes.
+  Write a method that determines if all the boxes can be opened.
 """
 
 
 def canUnlockAll(boxes):
     """
-      LockBoxes Function: Return True if all boxes can be opened, else false
+      Implementation
     """
-    T = []
-    for i in range(1, len(boxes)):
-        order = [T.extend(x) for x in boxes[:i] + boxes[i + 1:]]
-        if i in T:
-            T = []
-        else:
+
+    keys = [] + boxes[0]
+    need_keys = []
+
+    for box in range(1, len(boxes)):
+        if not keys:
             return False
-    return True
+        if box in keys:
+            keys = keys + boxes[box]
+            for box_prev in need_keys:
+                if box_prev in keys:
+                    keys = keys + boxes[box_prev]
+                    need_keys.pop(need_keys.index(box_prev))
+        else:
+            need_keys.append(box)
+    if not need_keys:
+        return True
+    else:
+        return False
