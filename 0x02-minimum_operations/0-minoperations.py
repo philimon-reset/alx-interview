4520afc462c4c88d1fc0fdb1317a580bec44df32
philimon-reset <philimonderib2019@gmail.com>
2021-12-09 18:06:35 +0300

    test 5  #

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,26 +1,46 @@
 #!/usr/bin/python3
 """ Minimum Operations
     """
-
+#!/usr/bin/python3
+""" Minimum Operations
+    """
 
 def minOperations(n: int) -> int:
     """ Minimum Operations needed to get n H characters """
+    flag = False
     if n < 2:
         return 0
+    for i in range(2, n):
+        if (n % i) == 0:
+            flag = True
+            break
+    if flag:
+        pass
+    else:
+        return n
     if n % 2 == 0:
         C = "H"
         jump = C
-        orand = 0
+        orand = 1
     else:
         C = "HHH"
         jump = "H"
         orand = 3
     while len(C) < n:
-        if len(C + jump) <= n and len(C + C) > n:
+        if len(C + jump) <= n and n % len(jump + C) == 0:
             orand += 1
             C += jump
+            check = len(C)
         else:
             jump = C
             orand += 2
             C += C
+            check = len(C)
     return orand
+
+if __name__ == '__main__':
+    n = 4
+    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+    n = 25
+    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
