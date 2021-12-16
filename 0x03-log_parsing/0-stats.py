5b59632964fc99f0e3015e14d922d5d3875abf42
philimon-reset <philimonderib2019@gmail.com>
2021-12-16 06:45:29 +0300

    log parser

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import sys
+import re
+
+match_B = re.compile("""\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{1,2}-\d{1,2} \d{2}:\d{2}:\d{2}\.\d+\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d{1,4})""")  # nopep8
+
+
+def log_parser():
+    cursor = 0
+    file_S = 0
+    status = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0}
+    try:
+        for i in sys.stdin:
+            result = re.match(match_B, i)
+            if result:
+                cursor += 1
+                status[result.group(1)] += 1
+                file_S += int(result.group(2))
+                if cursor % 10 == 0:
+                    print(f"File size: {file_S}")
+                    for key, value in status.items():
+                        print(f"{key}: {value}")
+    except KeyboardInterrupt:
+        print(f"File size: {file_S}")
+        for key, value in status.items():
+            print(f"{key}: {value}")
+
+
+if __name__ == '__main__':
+    log_parser()
