bd388b269bdc5ce623051426db1b6037c09ca67f
philimon-reset <philimonderib2019@gmail.com>
2021-12-16 07:10:06 +0300

    test #3

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,6 +2,7 @@
 
 import sys
 import re
+import signal
 
 match_B = re.compile("""\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{1,2}-\d{1,2} \d{2}:\d{2}:\d{2}\.\d+\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d{1,4})""")  # nopep8
 cursor = 0
@@ -15,18 +16,21 @@ status = {
     "404": 0,
     "405": 0,
     "500": 0}
-try:
-    for i in sys.stdin:
-        result = re.match(match_B, i)
-        if result:
-            cursor += 1
-            status[result.group(1)] += 1
-            file_S += int(result.group(2))
-            if cursor % 10 == 0:
-                print(f"File size: {file_S}")
-                for key, value in status.items():
-                    print(f"{key}: {value}")
-except KeyboardInterrupt:
+
+def signal_handler(sig, frame):
     print(f"File size: {file_S}")
     for key, value in status.items():
         print(f"{key}: {value}")
+
+signal.signal(signal.SIGINT, signal_handler)
+
+for i in sys.stdin:
+    result = re.match(match_B, i)
+    if result:
+        cursor += 1
+        status[result.group(1)] += 1
+        file_S += int(result.group(2))
+        if cursor % 10 == 0:
+            print(f"File size: {file_S}")
+            for key, value in status.items():
+                print(f"{key}: {value}")
