af77092971621c9ba31309639e176f95c3d4e7d1
philimon-reset <philimonderib2019@gmail.com>
2021-12-23 07:26:15 +0300

    check2

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" UTF-8 validation """
+
+
+def validUTF8(data):
+    """ main function """
+    flag = False
+    jump_list = {30: 3, 14: 4, 6: 5}
+    char_length = {3: 3, 4: 2, 5: 1}
+    list_length = 0
+    if len(data) == 1 and data[0] >> 6 == 1:
+        return True
+    for num in data:
+        if list_length:
+            list_length -= 1
+        else:
+            flag = False
+        if num >> 6 != 1 and len(bin(num)[2:]) >= 8:
+            flag = True
+            for test_point, shift in jump_list.items():
+                if num >> shift == test_point:
+                    list_length = char_length[shift]
+                else:
+                    return False
+        if num >> 6 == 1 and flag:
+            return False
+    return True
