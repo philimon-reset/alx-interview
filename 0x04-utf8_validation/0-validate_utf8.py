d177d65bb87d5ce6add086ef876ca5700ca9b943
philimon-reset <philimonderib2019@gmail.com>
2021-12-23 09:18:04 +0300

    check 2 #

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,9 +8,7 @@ def validUTF8(data):
     jump_list = {30: 3, 14: 4, 6: 5}
     char_length = {3: 3, 4: 2, 5: 1}
     list_length = 0
-    if len(data) == 0:
-        return True
-    if len(data) == 1 and data[0] >> 7 == 0:
+    if (len(data) == 0) or (len(data) == 1 and data[0] >> 7 == 0):
         return True
     for num in data:
         if list_length:
@@ -18,16 +16,20 @@ def validUTF8(data):
         else:
             flag = False
         if num >> 7 != 0 and len(bin(num)[2:]) >= 8:
-            if (not flag and num >> 6 == 2) or (flag and num >> 6 != 2):
+            if (not flag and num >> 6 == 2):
+                return False
+            elif (flag and num >> 6 != 2):
                 return False
             for test_point, shift in jump_list.items():
                 if num >> shift == test_point:
                     list_length = char_length[shift]
                     break
-            if not (list_length and flag):
+            if not (list_length or flag):
                 return False
             else:
                 flag = True
         elif num >> 7 == 0 and flag:
             return False
+    if list_length:
+        return False
     return True
