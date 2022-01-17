dae85ae74f587d03cedabf2dc6b1fbdac4de355b
philimon-reset <philimonderib2019@gmail.com>
2022-01-17 17:48:16 +0300

    current solution

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" -*- coding: rotate matrix """
+
+def rotate_2d_matrix(matrix):
+    """ rotate matrix - transpose """
+    N = len(matrix)
+    for row in range(N):
+        new = []
+        for col in range(N - 1, -1, -1):
+            new.append(matrix[col][row])
+        matrix.append(new)
+    [matrix.pop(0) for i in range(3)]
