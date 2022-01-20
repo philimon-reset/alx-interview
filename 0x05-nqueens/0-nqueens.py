6c793382be1c733c3a8cf7b8a062b3d8f5256d8c
philimon-reset <philimonderib2019@gmail.com>
2022-01-20 15:14:55 +0300

    rebase

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,94 +1,61 @@
 #!/usr/bin/python3
 """ N queens interview """
 
+from shutil import move
 import sys
 
-board = {}
 
-def make_board(dimension):
-    """ Generate board based on dimensions """
-    for row in range(0, dimension):
-        for column in range(0, dimension):
-            placement = (row, column)
-            board[placement] = "#"
-    return board
-
-
-def printing_board(dimension):
+def printing_board(dimension, board):
     """ function to print the board """
     i = 0
-    for keys, values in board.items():
+    for values in board.values():
         if i % dimension == 0 and i != 0:
             print()
         i += 1
-        print(f"| {values} |", end="")
-
-def main():
-    # if len(sys.argv) != 2:
-    #     print("Usage: nqueens N")
-    #     exit(1)
-    try:
-        dimension = 6
-        # if dimension < 4:
-        #     print("N must be at least 4")
-        #     exit(1)
-        safe = []
-        total_run = []
-        started = False
-        spots_left = dimension
-        for x in range(0, dimension):
-            for y in range(0, dimension):
-                if started == False:
-                    board = make_board(dimension)
-                    current = fill_block(dimension, (x,y), spots_left)
-                    spots_left -= 1
-                    if not current:
-                        board = make_board(dimension)
-                        spots_left = dimension
-                        total_run = []
-                    else:
-                        total_run.extend(((x,y), current))
-                        spots_left -= 1
-                        started = True
-                while spots_left > 0:
-                    current = fill_block(dimension, current, spots_left)
-                    spots_left -= 1
-                    total_run.append(current)
-                    print(total_run, "\n", "\n")
-                    if not current:
-                        board = make_board(dimension)
-                        spots_left = dimension
-                        total_run = []
-                        break
-                valid = True
-                if len(total_run) > 0:
-                    for i in range(len(safe)):
-                        if len(set(safe[i]) - set(total_run)) != 0:
-                            pass
-                        else:
-                            valid = False
-                            break
-                    if valid:
-                        safe.append(total_run)
-                    spots_left = dimension
-                    total_run = []
-                started = False
-        for i in safe:
-            print(i)
-    except Exception as E:
-        print("N must be a number", E)
-        exit(1)
+        print(f"|{values} |", end="")
 
 
+def validate(board, spots):
+    """ Valitate the board """
+    if len([i for i in board.values() if i == "#"]) < spots and spots != 0:
+        return False
+    return True
 
-def fill_block(dimension, index, spots_left):
+
+def solve(dimension, index, board, safe_T, spots_L):
+    """ main loop for recursive """
+    if len(safe_T) > dimension:
+        return False
+    moved_board = fill_block(dimension, index, board)
+    if validate(moved_board, spots_L) and moved_board:
+        spots_L -= 1
+        inital = (index[0] + 1, index[1])
+        for i in range(dimension):
+            try:
+                if moved_board[(inital[0], i)] == "#":
+                    safe_T.append((inital[0], i))
+                    if not solve(
+                            dimension, (inital[0], i), moved_board,
+                            safe_T, spots_L):
+                        safe_T.pop()
+                        continue
+            except BaseException:
+                continue
+    if spots_L < 0 and len(safe_T) == dimension:
+        print(safe_T)
+    else:
+        return False
+
+
+def fill_block(dimension, index, board_T):
     """ function to fill queens available spots """
+    board = board_T.copy()
     board[index] = 0
 # -------------------------------------------
     diagonal_C_neg_x = []
     diagonal_C_pos_x = []
     diagonal_C_neg_y = []
-    diagonal_C_pos_y= []
+    diagonal_C_pos_y = []
     x = index[1]
     while x - 1 >= 0:
         less_x = (index[0], x - 1)
@@ -133,10 +100,27 @@ def fill_block(dimension, index, spots_left):
         if board.get(ind) == 0:
             return False
         board[ind] = 1
-    if len([i for i in board.values() if i == "#"]) < spots_left and spots_left != 0:
-        return False
-    return [i for i in board.keys() if board[i] == "#"][0]
+    return board
 
 
-if __name__ == '__main__':
-    main()
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
+try:
+    dimension = int(sys.argv[1])
+    if dimension < 4:
+        print("N must be at least 4")
+        exit(1)
+    board = {}
+    for row in range(0, dimension):
+        for column in range(0, dimension):
+            placement = (row, column)
+            board[placement] = "#"
+    for i in range(0, dimension):
+        inital = (0, i)
+        safe_T = [inital]
+        spots_L = dimension - 1
+        solve(dimension, inital, board, safe_T, spots_L)
+except Exception as E:
+    print("N must be a number", E)
+    exit(1)
