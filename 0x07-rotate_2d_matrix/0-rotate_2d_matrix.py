7f6f236bde6257a1b88d7e09e2017949a12ce517
philimon-reset <philimonderib2019@gmail.com>
2022-01-20 06:22:49 +0300

    fix #

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -10,4 +10,4 @@ def rotate_2d_matrix(matrix) -> None:
         for col in range(N - 1, -1, -1):
             new.append(matrix[col][row])
         matrix.append(new)
-    [matrix.pop(0) for i in range(3)]
+    [matrix.pop(0) for i in range(N)]
