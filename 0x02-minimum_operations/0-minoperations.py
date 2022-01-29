f84bd3fcec5a7f83c3dbfe986d71b221fe025c29
philimon-reset <philimonderib2019@gmail.com>
2022-01-29 20:40:10 +0300

    werid how easy it was to solve #

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,33 +5,17 @@
 
 def minOperations(n: int) -> int:
     """ Minimum Operations needed to get n H characters """
-    flag = False
-    if n < 2 or type(n) != int:
-        return 0
-    for i in range(2, n):
-        if (n % i) == 0:
-            flag = True
-            break
-    if flag:
-        pass
-    else:
-        return n
-    if n % 2 == 0:
-        C = "H"
-        jump = C
-        orand = 1
-    else:
-        C = "HHH"
-        jump = "H"
-        orand = 3
-    while len(C) < n:
-        if len(C + jump) <= n and n % len(jump + C) == 0:
-            orand += 1
-            C += jump
-            check = len(C)
+    next = 'H'
+    body = 'H'
+    op = 0
+    while (len(body) < n):
+        if n % len(body) == 0:
+            op += 2
+            next = body
+            body += body
         else:
-            jump = C
-            orand += 2
-            C += C
-            check = len(C)
-    return orand
+            op += 1
+            body += next
+    if len(body) != n:
+        return 0
+    return op
